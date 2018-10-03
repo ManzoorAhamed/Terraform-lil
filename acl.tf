f867ed62a9e3305e72066ae335b5848403ed0c53
Manzoor Ahamed Syed Rahim <manzoor.ahamed@timeinc.com>
2018-10-03 10:12:45 +0530

    Adding acl and subnetworks comments

--- acl.tf
+++ acl.tf
@@ -2,14 +2,6 @@ resource "aws_network_acl" "acl-1" {
 	vpc_id = "${aws_vpc.environment-example-2.id}"
 	subnet_ids = ["${aws_subnet.subnet1-pub.id}","${aws_subnet.subnet2-pub.id}"]
 
-	egress {
-		protocol = "tcp"
-		cidr_block = "0.0.0.0/0"
-		rule_no  = "10"
-		from_port = "22"
-		to_port = "22"
-		action = "allow"
-	}
 
 	ingress {	
 		protocol = "tcp"
@@ -20,7 +12,28 @@ resource "aws_network_acl" "acl-1" {
 		action = "allow"
 	}
 
+	ingress {
+                protocol = "tcp"
+                cidr_block = "0.0.0.0/0"
+                rule_no = "11"
+                from_port = "22"
+                to_port = "22"
+                action = "allow"
+        }
+
 	tags {
 		name = "TF-acl"
 	}
+}
+
+
+# Allowing outboud rule through ACL
+
+resource "aws_network_acl_rule" "net_acl_rule_egress" {
+  network_acl_id = "${aws_network_acl.acl-1.id}"
+  cidr_block     = "0.0.0.0/0"
+  protocol       = "-1"
+  rule_number    = "100"
+  rule_action    = "allow"
+  egress         = "true"
 }
--- subnetworks.tf
+++ subnetworks.tf
@@ -1,7 +1,10 @@
+# Public Subnet
+
 resource "aws_subnet" "subnet1-pub" {
 	cidr_block = "10.0.0.0/28"
 	vpc_id = "${aws_vpc.environment-example-2.id}"
 	availability_zone = "us-west-2a"
+	map_public_ip_on_launch = true
 	
 	tags {
 		Name = "subnet1-pub"
@@ -12,11 +15,14 @@ resource "aws_subnet" "subnet2-pub" {
 	cidr_block = "10.0.0.16/28"
 	vpc_id = "${aws_vpc.environment-example-2.id}"
 	availability_zone = "us-west-2b"
+	map_public_ip_on_launch = true
 
 	tags {
                 Name = "subnet2-pub"
         }
 }
+
+# Private Subnet
 
 resource "aws_subnet" "subnet3-prv" {
 	cidr_block = "10.0.0.32/28"
